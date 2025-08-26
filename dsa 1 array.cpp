54ac82cd3819db2c0084a9f852c73df118e2f355
Amna-Luqman <63508@students.riphah.edu.pk>
2025-08-26 18:45:42 +0500

    Add files via upload

---
+++ dsa 1 array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string arr[6];  // Declare array of size 6
+
+    // Take input from the user
+    cout << "Enter 6 names:" << endl;
+    for (int i = 0; i < 6; i++) {
+       cout << "Enter name "<<i+1 << "  ";
+	    cin >> arr[i];
+    }
+
+    // Display the array
+    cout << "You entered: ";
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
