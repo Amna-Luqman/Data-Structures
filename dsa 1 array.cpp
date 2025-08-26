35e4d4a657ce6d23bcae5183d3abc76b7bbd2ee6
Amna-Luqman <63508@students.riphah.edu.pk>
2025-08-26 18:50:36 +0500

    Update dsa 1 array.cpp

--- dsa 1 array.cpp
+++ dsa 1 array.cpp
@@ -3,15 +3,12 @@ using namespace std;
 
 int main() {
    string arr[6];  
-
-    // Take input from the user
+   
     cout << "Enter 6 names:" << endl;
     for (int i = 0; i < 6; i++) {
        cout << "Enter name "<<i+1 << "  ";
 	    cin >> arr[i];
     }
-
-    // Display the array
     cout << "You entered: ";
     for (int i = 0; i < 6; i++) {
         cout << arr[i] << " ";
@@ -19,5 +16,6 @@ int main() {
 
     return 0;
 }
+
 
 
